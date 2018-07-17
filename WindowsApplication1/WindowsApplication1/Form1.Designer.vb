010b89c1cc934ec02141c3abe23b7777c731b391
Priyansh Shah <41307626+priyanshshah42@users.noreply.github.com>
2018-07-17 14:29:09 -0400

    Add files via upload

---
+++ WindowsApplication1/WindowsApplication1/Form1.Designer.vb
@@ -0,0 +1,133 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.InputTextBox = New System.Windows.Forms.TextBox()
+        Me.TitleLabel = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.ConvertButton = New System.Windows.Forms.Button()
+        Me.OutputLabel = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.BaseTextBox = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'InputTextBox
+        '
+        Me.InputTextBox.Location = New System.Drawing.Point(526, 72)
+        Me.InputTextBox.Name = "InputTextBox"
+        Me.InputTextBox.Size = New System.Drawing.Size(117, 20)
+        Me.InputTextBox.TabIndex = 0
+        '
+        'TitleLabel
+        '
+        Me.TitleLabel.AutoSize = True
+        Me.TitleLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TitleLabel.Location = New System.Drawing.Point(205, 9)
+        Me.TitleLabel.Name = "TitleLabel"
+        Me.TitleLabel.Size = New System.Drawing.Size(268, 26)
+        Me.TitleLabel.TabIndex = 1
+        Me.TitleLabel.Text = "Numerical Base Converter"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(13, 54)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(301, 15)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Enter the numerical base you wish to convert to (2-62):"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(523, 54)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(120, 13)
+        Me.Label2.TabIndex = 11
+        Me.Label2.Text = "Enter a base 10 integer:"
+        '
+        'ConvertButton
+        '
+        Me.ConvertButton.Location = New System.Drawing.Point(568, 98)
+        Me.ConvertButton.Name = "ConvertButton"
+        Me.ConvertButton.Size = New System.Drawing.Size(75, 23)
+        Me.ConvertButton.TabIndex = 12
+        Me.ConvertButton.Text = "Convert"
+        Me.ConvertButton.UseVisualStyleBackColor = True
+        '
+        'OutputLabel
+        '
+        Me.OutputLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.OutputLabel.Location = New System.Drawing.Point(330, 152)
+        Me.OutputLabel.Name = "OutputLabel"
+        Me.OutputLabel.Size = New System.Drawing.Size(313, 113)
+        Me.OutputLabel.TabIndex = 13
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(330, 132)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(42, 13)
+        Me.Label3.TabIndex = 14
+        Me.Label3.Text = "Output:"
+        '
+        'BaseTextBox
+        '
+        Me.BaseTextBox.Location = New System.Drawing.Point(16, 72)
+        Me.BaseTextBox.Name = "BaseTextBox"
+        Me.BaseTextBox.Size = New System.Drawing.Size(19, 20)
+        Me.BaseTextBox.TabIndex = 15
+        Me.BaseTextBox.Text = "2"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(655, 268)
+        Me.Controls.Add(Me.BaseTextBox)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.OutputLabel)
+        Me.Controls.Add(Me.ConvertButton)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TitleLabel)
+        Me.Controls.Add(Me.InputTextBox)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents InputTextBox As TextBox
+    Friend WithEvents TitleLabel As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents ConvertButton As Button
+    Friend WithEvents OutputLabel As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents BaseTextBox As TextBox
+End Class
---
+++ WindowsApplication1/WindowsApplication1/Form1.vb
@@ -0,0 +1,190 @@
+﻿Public Class Form1
+
+    Private Sub ConvertButton_Click(sender As Object, e As EventArgs) Handles ConvertButton.Click
+
+        Dim n As Long
+        Dim nCopy As String
+        Dim output As String = ""
+        Dim base As Short
+        Dim result As Long
+        Dim remainder As String
+        Dim numberOfDigitsTillSpace As Integer = 4
+
+        n = Val(InputTextBox.Text)
+
+        If n > 0 Then
+
+            n = Int(InputTextBox.Text)
+            nCopy = n
+
+            base = Val(BaseTextBox.Text)
+
+            If base >= 2 And base <= 62 Then
+
+                base = Int(BaseTextBox.Text)
+
+                If base = 8 Then
+
+                    numberOfDigitsTillSpace = 3
+
+                End If
+
+            Else
+
+                MessageBox.Show("Please enter an integer equal to or between 2 and 62", "Error", MessageBoxButtons.OK)
+
+            End If
+
+            While n <> 0
+
+                result = Int(n / base)
+                remainder = Int(n Mod base)
+                n = result
+
+#Region "Remainders"
+
+                If remainder = "10" Then
+                    remainder = "A"
+                ElseIf remainder = "11" Then
+                    remainder = "B"
+                ElseIf remainder = "12" Then
+                    remainder = "C"
+                ElseIf remainder = "13" Then
+                    remainder = "D"
+                ElseIf remainder = "14" Then
+                    remainder = "E"
+                ElseIf remainder = "15" Then
+                    remainder = "F"
+                ElseIf remainder = "16" Then
+                    remainder = "G"
+                ElseIf remainder = "17" Then
+                    remainder = "H"
+                ElseIf remainder = "18" Then
+                    remainder = "I"
+                ElseIf remainder = "19" Then
+                    remainder = "J"
+                ElseIf remainder = "20" Then
+                    remainder = "K"
+                ElseIf remainder = "21" Then
+                    remainder = "L"
+                ElseIf remainder = "22" Then
+                    remainder = "M"
+                ElseIf remainder = "23" Then
+                    remainder = "N"
+                ElseIf remainder = "24" Then
+                    remainder = "O"
+                ElseIf remainder = "25" Then
+                    remainder = "P"
+                ElseIf remainder = "26" Then
+                    remainder = "Q"
+                ElseIf remainder = "27" Then
+                    remainder = "R"
+                ElseIf remainder = "28" Then
+                    remainder = "S"
+                ElseIf remainder = "29" Then
+                    remainder = "T"
+                ElseIf remainder = "30" Then
+                    remainder = "U"
+                ElseIf remainder = "31" Then
+                    remainder = "V"
+                ElseIf remainder = "32" Then
+                    remainder = "W"
+                ElseIf remainder = "33" Then
+                    remainder = "X"
+                ElseIf remainder = "34" Then
+                    remainder = "Y"
+                ElseIf remainder = "35" Then
+                    remainder = "Z"
+                ElseIf remainder = "36" Then
+                    remainder = "a"
+                ElseIf remainder = "37" Then
+                    remainder = "b"
+                ElseIf remainder = "38" Then
+                    remainder = "c"
+                ElseIf remainder = "39" Then
+                    remainder = "d"
+                ElseIf remainder = "40" Then
+                    remainder = "e"
+                ElseIf remainder = "41" Then
+                    remainder = "f"
+                ElseIf remainder = "42" Then
+                    remainder = "g"
+                ElseIf remainder = "43" Then
+                    remainder = "h"
+                ElseIf remainder = "44" Then
+                    remainder = "i"
+                ElseIf remainder = "45" Then
+                    remainder = "j"
+                ElseIf remainder = "46" Then
+                    remainder = "k"
+                ElseIf remainder = "47" Then
+                    remainder = "l"
+                ElseIf remainder = "48" Then
+                    remainder = "m"
+                ElseIf remainder = "49" Then
+                    remainder = "n"
+                ElseIf remainder = "50" Then
+                    remainder = "o"
+                ElseIf remainder = "51" Then
+                    remainder = "p"
+                ElseIf remainder = "52" Then
+                    remainder = "q"
+                ElseIf remainder = "53" Then
+                    remainder = "r"
+                ElseIf remainder = "54" Then
+                    remainder = "s"
+                ElseIf remainder = "55" Then
+                    remainder = "t"
+                ElseIf remainder = "56" Then
+                    remainder = "u"
+                ElseIf remainder = "57" Then
+                    remainder = "v"
+                ElseIf remainder = "58" Then
+                    remainder = "w"
+                ElseIf remainder = "59" Then
+                    remainder = "x"
+                ElseIf remainder = "60" Then
+                    remainder = "y"
+                ElseIf remainder = "61" Then
+                    remainder = "z"
+                End If
+
+#End Region
+
+                If base <> 8 And numberOfDigitsTillSpace = 0 Then
+                    output = remainder & " " & output
+                    numberOfDigitsTillSpace = 3
+                ElseIf base = 8 And numberOfDigitsTillSpace = 0 Then
+                    output = remainder & " " & output
+                    numberOfDigitsTillSpace = 2
+                Else
+                    output = remainder & output
+                    numberOfDigitsTillSpace -= 1
+                End If
+
+            End While
+
+            If base = 2 Then
+
+                If numberOfDigitsTillSpace = 3 Then
+                    output = "000" & output
+                ElseIf numberOfDigitsTillSpace = 2 Then
+                    output = "00" & output
+                ElseIf numberOfDigitsTillSpace = 1 Then
+                    output = "0" & output
+                End If
+
+            End If
+
+            Label3.Text = nCopy & " in decimal when converted to base " & base & " is:"
+            OutputLabel.Text = output
+
+        Else
+
+            MessageBox.Show("Please enter a positive integer", "Error", MessageBoxButtons.OK)
+
+        End If
+
+    End Sub
+
+End Class
